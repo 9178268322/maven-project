51a41143b49a9703d8be4a156488d82dc2a669f3
Your Name <you@example.com>
2020-04-09 13:35:01 -0700

    first commit

---
+++ src/test/java/com/wbl/jenkins/JenkinsMavenBasedProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbl.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsMavenBasedProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
