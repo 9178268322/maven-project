c3b72b1c0fac1f01731a40401dc26dd8ca6d415b
Your Name <you@example.com>
2020-04-11 13:01:38 -0700

    hope

--- src/main/java/com/wbl/jenkins/JenkinsMavenBasedProjectApplication.java
+++ src/main/java/com/wbl/jenkins/JenkinsMavenBasedProjectApplication.java
@@ -2,25 +2,26 @@ package com.wbl.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinsMavenBasedProjectApplication {
+public class JenkinsMavenBasedProjectApplication extends SpringBootServletInitializer{
 
 	public static void main(final String[] args) {
 		SpringApplication.run(JenkinsMavenBasedProjectApplication.class, args);
 	}
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	        return builder.sources(JenkinsMavenBasedProjectApplication.class);
+	    }
 	
 	@GetMapping(value = "/get")
 	public String test() {
 		return "Hello, World";
-	}
-	
-	@GetMapping(value = "/hello")
-	public String test1() {
-		return "Jenkins 1";
 	}
 
 }
